b7278f9881d4d29dccacf775fd35285aa5e9615c
colly-m <sindanicolly@gmail.com>
2024-04-25 20:55:16 +0000

    Aeropuerto update fix

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,5 +1,6 @@
 export default class Airport {
   constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
     this._name = name;
     this._code = code;
   }
