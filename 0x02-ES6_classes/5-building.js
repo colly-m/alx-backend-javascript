136c45003119d5161d81e9680e1605e8b85dd453
colly-m <sindanicolly@gmail.com>
2024-04-25 20:25:50 +0000

    Heir update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
-    this.evacuationWarningMessagw();
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -13,6 +13,7 @@ export default class Building {
     this._sqft = value;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
