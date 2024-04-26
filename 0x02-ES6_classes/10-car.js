3367ebfcd833b168924f47e9558578536e0ff454
colly-m <sindanicolly@gmail.com>
2024-04-26 14:04:30 +0000

    Skrt skrt

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,24 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+}
