7bd596f8354eeb2cede2aaa0180104495e9e9a06
colly-m <sindanicolly@gmail.com>
2024-04-26 20:59:07 +0000

    Aeropuerto update fix

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -8,16 +8,8 @@ export default class Airport {
     return this._name;
   }
 
-  set name(value) {
-    this._name = value;
-  }
-
   get code() {
     return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
   }
 
   toString() {
