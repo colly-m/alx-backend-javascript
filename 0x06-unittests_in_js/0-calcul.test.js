c3076f2bf64e04f44440256699d86e21ac2f917b
colly-m <sindanicolly@gmail.com>
2024-06-20 21:53:17 +0000

    Basic Mocha  test with assert

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const {it, describe} = require('mocha');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
+  });
+
+  it('round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
+  });
+
+  it('should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
+  });
+});
