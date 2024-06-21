8d501e479e9142f56d5a013cb141fa4115f865d6
colly-m <sindanicolly@gmail.com>
2024-06-21 21:30:41 +0000

    Watch your back

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,31 +1,18 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const { it, describe } = require('mocha');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  let calculateNumStub;
-  let consoleLogSpy;
-
-  beforeEach(() => {
-    calculateNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    consoleLogSpy = sinon.spy(console, 'log');
-  });
-
-  afterEach(() => {
-    calculateNumStub.restore();
-    consoleLogSpy.restore();
-  
-
-  it('should validate Utils.calculateNumber with "SUM", 100, and 20', () => {
+describe('', () => {
+  const checkSoy = sinon.spy(console, 'log');
+  it('checks if numbers round with spies and stubs', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumStub.calledOnce).to.be.true;
-    expect(calculateNumStub.calledWith('SUM', 100, 20)).to.be.true;
-  });
-
-  it('should log "The total is: 10"', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(consoleLogSpy.calledOnce).to.be.true;
-    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    const stubBoy = sinon.stub(Utils, 'calculateNumber');
+    stubBoy.withArgs('SUM', 100, 20).returns(120);
+    expect(checkSoy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    checkSoy.restore();
+    stubBoy.restore();
   });
 });
