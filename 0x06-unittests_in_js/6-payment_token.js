2ebf11548061094bf5573ca5f1a6097e2c33c131
colly-m <sindanicolly@gmail.com>
2024-08-12 07:25:51 -0700

    Dir removal

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,7 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-  if (success) {
-    return Promise.resolve({ data: 'Successful response from the API' });
-  }
-}
-
-module.exports = getPaymentTokenFromAPI;
