5c70f6076274e24865a0e36a4ea7bdab2be46668
colly-m <sindanicolly@gmail.com>
2024-08-22 15:34:46 -0700

    Utility spy update

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi function', () => {
+  it('should call Utils.calculateNumber with SUM and correct arguments', function() {
+    const utilSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(utilSpy.calledOnce).to.be.true;
+    expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    utilSpy.restore();
+  });
+});
