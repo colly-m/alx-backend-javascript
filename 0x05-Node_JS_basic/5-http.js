d786b2bc4aa7eb3c0d64f6bacdc8657ce4868192
colly-m <sindanicolly@gmail.com>
2024-08-23 15:35:40 -0700

    Express basic http update fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,20 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2]; // The database file is passed as a command-line argument
-
-const app = http.createServer(async (req, res) => {
-    if (req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        
-        try {
-            const studentList = await countStudents(database);
-            res.end(`This is the list of our students\n${studentList}`);
-        } catch (error) {
-            res.statusCode = 500;
-            res.end('Cannot load the database');
-        }
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Not Found');
-    }
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
+  }
 });
 
 app.listen(1245, () => {
